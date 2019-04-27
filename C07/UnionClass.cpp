374ad38a16c874d8555e660d15af1a35495974ec
zhouyong10 <1059981087@qq.com>
2019-04-27 16:32:14 +0800

    C07前两个示例

---
+++ C07/UnionClass.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+union U{
+private:
+	int i;
+	float f;
+public:
+	U(int a);
+	U(float f);
+	~U();
+	int read_int();
+	float read_float();	
+};
+
+U::U(int a){
+	i = a;
+}
+
+U::U(float b){
+	f = b;
+}
+
+U::~U(){
+	cout << "U::~U()\n";
+}
+
+int U::read_int(){
+	return i;
+}
+
+float U::read_float(){
+	return f;
+}
+
+
+int main(){
+	U X(12), Y(1.9f);
+	cout << X.read_int() << endl;
+	cout << Y.read_float() << endl;
+}
